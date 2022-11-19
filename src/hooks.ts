19b7756a11bceadd949dcca88df5300ca2442e38
MMammadov <mmehdi.mamedov@gmail.com>
2022-11-19 15:02:55 +0400

    remove project

--- src/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "react-query";
-import { carService } from "./services/carService";
-
-export function useCarMakes() {
-    const query = useQuery('carService.getMakes', carService.getMakes);
-
-    return query;    
-}
-
